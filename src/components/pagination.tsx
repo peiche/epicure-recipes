d18762a264c5f22f7d139a615b742eb8498c0bc0
Paul <peiche@gmail.com>
2025-02-04 20:38:23 -0600

    Component order fix

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,4 +1,4 @@
-import { Box, Link, Pagination as MuiPagination, PaginationItem } from "@mui/material";
+import { Box, Pagination as MuiPagination, PaginationItem } from "@mui/material";
 import NextLink from 'next/link';
 
 interface PaginationProps {
@@ -13,14 +13,18 @@ const Pagination = ({ prefix, currentPage, totalPages }: PaginationProps) => {
             <MuiPagination
                 count={totalPages}
                 page={currentPage}
+                showFirstButton
+                showLastButton
                 renderItem={(item) => (
                     (item.page === null || item.disabled === true) ? (
                         <PaginationItem {...item} />
                     ) : (
-                        <NextLink href={`${prefix}/${item.page === 1 ? '' : `${item.page}`}`}>
-                            {/* {JSON.stringify(item)} */}
-                            <PaginationItem {...item} color='primary' />
-                        </NextLink>
+                        <PaginationItem
+                            {...item}
+                            color='primary'
+                            component={NextLink}
+                            href={`${prefix}/${item.page === 1 ? '' : `${item.page}`}`}
+                        />
                     )
                 )}
             />
