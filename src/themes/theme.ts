5a3758a695e83214795796e4976c1f63471af067
Paul <peiche@gmail.com>
2025-02-04 20:38:34 -0600

    Default theme updates

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -1,5 +1,15 @@
 import { createTheme } from "@mui/material";
 
+const baseTheme = createTheme({
+    colorSchemes: {
+        light: true,
+        dark: true,
+    },
+    cssVariables: {
+        colorSchemeSelector: 'class',
+    },
+});
+
 const theme = createTheme({
     colorSchemes: {
         light: true,
@@ -7,6 +17,27 @@ const theme = createTheme({
     },
     cssVariables: {
         colorSchemeSelector: 'class',
+    },
+    components: {
+        MuiPaginationItem: {
+            styleOverrides: {
+                firstLast: {
+                    [baseTheme.breakpoints.up('sm')]: {
+                        display: 'none',
+                    },
+                },
+                page: {
+                    [baseTheme.breakpoints.down('sm')]: {
+                        display: 'none',
+                    },
+                },
+                ellipsis: {
+                    [baseTheme.breakpoints.down('sm')]: {
+                        display: 'none',
+                    },
+                },
+            },
+        },
     },
 });
 
