bde0151eba95e534be9313ba4ce0987891e429db
Paul <peiche@gmail.com>
2025-02-11 14:24:54 -0600

    Fixed Algolia warnings

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -4,6 +4,14 @@ import Wrapper from '../components/wrapper';
 import { searchClient, searchIndexName } from '../services/api/algolia';
 import SEO from '../components/seo';
 import SearchBoxResults from '../components/searchBoxResults';
+import { history } from 'instantsearch.js/es/lib/routers'
+import { RouterProps } from 'instantsearch.js/es/middlewares';
+
+const routing: RouterProps = {
+    router: history({
+        cleanUrlOnDispose: false,
+    }),
+};
 
 export default function Search() {
     return (
@@ -13,8 +21,11 @@ export default function Search() {
                 <InstantSearch
                     searchClient={searchClient}
                     indexName={searchIndexName}
-                    routing={true}
+                    routing={routing}
                     insights={false}
+                    future={{
+                        preserveSharedStateOnUnmount: true,
+                    }}
                 >
                     <Configure ruleContexts={[]} />
                     <SearchBoxResults />
