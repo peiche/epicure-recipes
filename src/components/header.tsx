ab4cbbfc8272b038f790a3613d8c7143b30a457b
Paul <peiche@gmail.com>
2025-03-19 21:52:15 -0500

    Removed donate button because whatever

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,17 +15,6 @@ export default function Header() {
             </AppBarSection>
             <AppBarSpacer />
             <AppBarSection className="k-gap-sm">
-                <a
-                    href='https://www.paypal.com/donate/?business=SY2SCFJENKDEJ&no_recurring=0&item_name=I%27m+providing+this+recipe+website+for+free.+If+you%27d+like+to+show+your+appreciation%2C+you+can+donate+the+amount+of+your+choice.&currency_code=USD'
-                    target="_blank"
-                    className="k-button k-button-md k-button-solid k-button-solid-base k-rounded-full"
-                    style={{
-                        backgroundColor: '#ffc439',
-                        color: '#2C2E2F',
-                        border: 0,
-                        paddingLeft: 12,
-                        paddingRight: 12
-                    }}>Donate</a>
                 <ModeToggle />
                 <SearchDialogButton />
             </AppBarSection>
