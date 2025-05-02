fa0123017bc893374b1efe24dc91413797dd921b
Paul <peiche@gmail.com>
2025-05-02 07:25:44 -0500

    Re-enabled auto ads

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,17 @@ import { Provider } from "react-redux";
 import { store, persistor } from "../redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { GoogleAnalytics } from '@next/third-parties/google';
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Script
+                id="Adsense-id"
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8316336599094727"
+                crossOrigin="anonymous"
+            />
             <Provider store={store}>
                 <PersistGate loading={null} persistor={persistor}>
                     <Component {...pageProps} />
